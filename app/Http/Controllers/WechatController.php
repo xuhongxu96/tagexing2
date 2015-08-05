b94fa5fae4470899d7a13aa978766a17879edbfe
xuhongxu <xuhongxu96@qq.com>
2015-08-05 16:59:11 +0800

    subscribe message

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -22,6 +22,12 @@ class WechatController extends Controller
 	public function serve(Server $server) {
 
 		$server->on('event', 'subscribe', function ($event) {
+			return <<<EOT
+欢迎来到imall公益电商平台 和 踏鸽行公共自行车服务平台！
+
+<a href="http://imall365.org">点此开始imall公益电商之旅！</a>
+<a href="">点此开始踏鸽行！</a>
+EOT;
 		});
 
 		$server->on('message', function ($message) {
