4f400dd7368220731df971e92743addb486a987c
xuhongxu <xuhongxu96@qq.com>
2015-08-05 19:25:23 +0800

    get openid

--- app/User.php
+++ app/User.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     //
+	protected $fillable = [
+		'openid',
+		'name',
+		'gender',
+		'email',
+		'mobile',
+		'qq',
+		'state',
+		'free_at',
+		'score',
+		'total',
+		'message',
+		'auth',
+		'school',
+		'student_id',
+		'department',
+	];
 }
