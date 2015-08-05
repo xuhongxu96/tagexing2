a3e4790b117c2baf1654865e8d959296623b7489
xuhongxu <xuhongxu96@gmail.com>
2015-08-05 11:48:39 +0800

    config updated

--- config/app.php
+++ config/app.php
@@ -144,6 +144,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+		Overtrue\LaravelWechat\ServiceProvider::class,
+
     ],
 
     /*
---
+++ config/wechat.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'use_alias'    => false,
+    'app_id'       => 'wx0da4ce589c21a7d7', // 必填
+    'secret'       => '58ca2b0e40815a9cf8a991bfa87e37f7', // 必填
+    'token'        => 'tagexing',  // 必填
+    'encoding_key' => 'YourEncodingAESKey' // 加密模式需要，其它模式不需要
+];
