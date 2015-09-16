d7abff4bd5f185f0b21a3841664901e805860c49
xuhongxu <xuhongxu96@qq.com>
2015-09-17 02:07:08 +0800

    new server

--- config/wechat.php
+++ config/wechat.php
@@ -1,8 +1,10 @@
 <?php
 return [
     'use_alias'    => false,
-    'app_id'       => 'wx0da4ce589c21a7d7', // 必填
-    'secret'       => '58ca2b0e40815a9cf8a991bfa87e37f7', // 必填
+    //'app_id'       => 'wx0da4ce589c21a7d7', // 必填
+    //'secret'       => '58ca2b0e40815a9cf8a991bfa87e37f7', // 必填
+    'app_id'       => 'wx8ffe190441f2b0a1', // 必填
+    'secret'       => '455a953bf16d062eb2c47943f6bc719f', // 必填
     'token'        => 'tagexing',  // 必填
-    'encoding_key' => 'YourEncodingAESKey' // 加密模式需要，其它模式不需要
+    'encoding_key' => '6pnP7qHyqJ1kFXMjuO4Z3QrpOa9WfapsgkPOtXoZKC2' // 加密模式需要，其它模式不需要
 ];
