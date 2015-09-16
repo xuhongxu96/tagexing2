b2c6225ce7a674f550cb984e67be7557936d3c87
xuhongxu <xuhongxu96@qq.com>
2015-09-17 02:15:22 +0800

    try fix img msg

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 
 use Overtrue\Wechat\Server;
 use Overtrue\Wechat\Media;
+use Overtrue\Wechat\Message;
 
 use App\User;
 use App\Rank;
@@ -48,13 +49,12 @@ class WechatController extends Controller
 EOT;
 		});
 
-		$server->on('message', function ($message) {
+		$server->on('message', 'text', function ($message) {
 			$url = action('IndexController@redirect');
-			if ($message->MsgType == "text") {
-				switch ($message->Content)
-				{
-				case "1":
-				return <<<EOT
+			switch ($message->Content)
+			{
+			case "1":
+			return <<<EOT
 imall，北师大社区慈善商店。www.imall365.org是我们的网站（即将面临改版）。
 欢迎关注我们的微信号”imall_dreammore”。
 【我们立志于】
@@ -69,29 +69,28 @@ imall，北师大社区慈善商店。www.imall365.org是我们的网站（即�
 4.  结识一群志同道合的优秀伙伴、前辈；
 5.  最重要的，imall会助你成长，让你的大学与众不同。
 EOT;
-					break;
-				case "2":
-				return <<<EOT
+				break;
+			case "2":
+			return <<<EOT
 <a href="https://jinshuju.net/f/z0GASP">imall报名</a>
 EOT;
-					break;
-				case "3":
-				return <<<EOT
+				break;
+			case "3":
+			return <<<EOT
 <a href="https://jinshuju.net/f/5uQ0Nn">imall调研</a>
 EOT;
-					break;
-				case "4":
-				return <<<EOT
+				break;
+			case "4":
+			return <<<EOT
 <a href="https://jinshuju.net/f/tKROez">踏鸽行报名</a>
 EOT;
-					break;
-				case "5":
-					$media = new Media(Config::get('wechat.app_id'), Config::get('wechat.secret'));
-					$imageId = $media->image(__DIR__ . '/qr.jpg'); // 上传并返回媒体ID
+				break;
+			case "5":
+				$media = new Media(Config::get('wechat.app_id'), Config::get('wechat.secret'));
+				$imageId = $media->image(__DIR__ . '/qr.jpg'); // 上传并返回媒体ID
 
-					return Message::make('image')->media($imageId);
-					break;
-				}
+				return Message::make('image')->media($imageId);
+				break;
 			}
 		});
 
